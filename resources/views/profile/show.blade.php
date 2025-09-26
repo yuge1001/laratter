6ad1370ccbd7a681b766211b44f9338c7cafc72a
yuge1001 <kouki20051001@icloud.com>
2025-09-26 19:36:20 +0900

    フォロー機能のテスト

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -14,6 +14,64 @@
           <div class="text-gray-600 dark:text-gray-400 text-sm">
             <p>アカウント作成日時: {{ $user->created_at->format('Y-m-d H:i') }}</p>
           </div>
+          @if ($user->id !== auth()->id())
+          <div class="text-gray-900 dark:text-gray-100">
+            @if ($user->followers->contains(auth()->id()))
+            <form action="{{ route('follow.destroy', $user) }}" method="POST">
+              @csrf
+              @method('DELETE')
+              <button type="submit" class="text-red-500 hover:text-red-700">unFollow</button>
+            </form>
+            @else
+            <form action="{{ route('follow.store', $user) }}" method="POST">
+              @csrf
+              <button type="submit" class="text-blue-500 hover:text-blue-700">follow</button>
+            </form>
+            @endif
+          </div>
+          @endif
+
+          <p>following: {{$user->follows->count()}}</p>
+          <p>followers: {{$user->followers->count()}}</p>
+
+          @if ($tweets->count())
+
+          <div class="mb-4">
+            {{ $tweets->appends(request()->input())->links() }}
+          </div>
+
+          @foreach ($tweets as $tweet)
+          <div class="mb-4 p-4 bg-gray-100 dark:bg-gray-700 rounded-lg">
+            <p class="text-gray-800 dark:text-gray-300">{{ $tweet->tweet }}</p>
+            <a href="{{ route('profile.show', $tweet->user) }}">
+              <p class="text-gray-600 dark:text-gray-400 text-sm">投稿者: {{ $tweet->user->name }}</p>
+            </a>
+            <a href="{{ route('tweets.show', $tweet) }}" class="text-blue-500 hover:text-blue-700">詳細を見る</a>
+            <div class="flex">
+              @if ($tweet->liked->contains(auth()->id()))
+              <form action="{{ route('tweets.dislike', $tweet) }}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button type="submit" class="text-red-500 hover:text-red-700">dislike {{ $tweet->liked->count() }}</button>
+              </form>
+              @else
+              <form action="{{ route('tweets.like', $tweet) }}" method="POST">
+                @csrf
+                <button type="submit" class="text-blue-500 hover:text-blue-700">like {{ $tweet->liked->count() }}</button>
+              </form>
+              @endif
+            </div>
+          </div>
+          @endforeach
+
+          <!-- ページネーション -->
+          <div class="mt-4">
+            {{ $tweets->appends(request()->input())->links() }}
+          </div>
+
+          @else
+          <p>No tweets found.</p>
+          @endif
         </div>
       </div>
     </div>
